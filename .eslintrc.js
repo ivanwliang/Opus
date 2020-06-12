407eb0ef196a34055c5c70fae10793f00c24ebc0
Ivan Liang <ivanwenliang@gmail.com>
2020-06-12 16:34:07 -0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'get-off-my-lawn',
+  rules: {
+    'no-param-reassign': 0,
+    'react/display-name': 0,
+    'react/jsx-no-literals': 0,
+    'react/jsx-sort-props': 0,
+    'react/prop-types': 0
+  }
+}
