e0104d2192199ede3ac93a3e800c1803885f17ce
Ivan Liang <ivanwenliang@gmail.com>
2020-06-12 20:09:40 -0700

    add tailwind

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,12 @@ module.exports = {
   rules: {
     'no-param-reassign': 0,
     'react/display-name': 0,
+    'react/forbid-elements': 0,
     'react/jsx-no-literals': 0,
     'react/jsx-sort-props': 0,
-    'react/prop-types': 0
+    'react/prop-types': 0,
+    'sort-keys': 0,
+    'node/no-unpublished-require': 0,
+    'jsx-a11y/anchor-is-valid': 0
   }
 }
---
+++ pages/_app.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../styles/index.css'
+
+const GlobalStyles = ({ children }) => {
+  return <div className="bg-gray-50">{children}</div>
+}
+
+// eslint-disable-next-line get-off-my-lawn/prefer-arrow-functions
+function MyApp({ Component, pageProps }) {
+  return (
+    <GlobalStyles>
+      <Component {...pageProps} />
+    </GlobalStyles>
+  )
+}
+
+/*
+ * Only uncomment this method if you have blocking data requirements for
+ * every single page in your application. This disables the ability to
+ * perform automatic static optimization, causing every page in your app to
+ * be server-side rendered.
+ *
+ * MyApp.getInitialProps = async (appContext) => {
+ *   // calls page's `getInitialProps` and fills `appProps.pageProps`
+ *   const appProps = await App.getInitialProps(appContext);
+ *
+ *   return { ...appProps }
+ * }
+ */
+
+export default MyApp
