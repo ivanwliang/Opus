3e2ab96c3122a99eb315e2bea34b5382c0a3968a
Ivan Liang <ivanwenliang@gmail.com>
2020-06-20 17:46:25 -0700

    refactor signin and signup forms

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
-
-import { useAuth } from '../hooks/use-auth'
+import { useRouter } from 'next/router'
 
 import Logo from './Logo'
 
 const Navbar = () => {
-  const auth = useAuth()
+  const router = useRouter()
 
   return (
     <nav className="bg-gray-50 pt-3">
@@ -17,18 +16,20 @@ const Navbar = () => {
           </div>
 
           {/* Navbar right side */}
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <Link href="/signin">
-              <span className="inline-flex rounded-md shadow-sm">
-                <button
-                  type="button"
-                  className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
-                >
-                  Sign In
-                </button>
-              </span>
-            </Link>
-          </div>
+          {router.pathname !== '/signup' && router.pathname !== '/signin' && (
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <Link href="/signin">
+                <span className="inline-flex rounded-md shadow-sm">
+                  <button
+                    type="button"
+                    className="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
+                  >
+                    Sign In
+                  </button>
+                </span>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </nav>
--- pages/signup.js
+++ pages/signup.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { useForm } from 'react-hook-form'
 
 import { useAuth } from '../hooks/use-auth'
 import PublicLayout from '../layouts/PublicLayout'
-import FullScreenAuth from '../components/Auth'
 
 export default () => {
   const auth = useAuth()
   const router = useRouter()
+
+  const { register, handleSubmit, errors } = useForm()
 
   const signUp = ({ email, password, name }) => {
     auth
@@ -20,11 +23,127 @@ export default () => {
 
   return (
     <PublicLayout>
-      <FullScreenAuth
-        type="Sign Up"
-        onSubmit={signUp}
-        header="Sign up for a new account"
-      />
+      <div className="min-h-full bg-gray-50 flex flex-col justify-center py-6 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-3 text-center text-3xl leading-9 font-extrabold text-gray-900">
+            Sign up for a new account
+          </h2>
+        </div>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form onSubmit={handleSubmit(signUp)}>
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium leading-5 text-gray-700"
+                >
+                  Name
+                </label>
+                <div className="mt-1 rounded-md shadow-sm">
+                  <input
+                    name="name"
+                    ref={register({ required: true })}
+                    id="name"
+                    type="text"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  />
+                </div>
+                {errors.name && (
+                  <span className="text-xs text-red-600">
+                    This field is required
+                  </span>
+                )}
+              </div>
+
+              <div className="mt-6">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-5 text-gray-700"
+                >
+                  Email address
+                </label>
+                <div className="mt-1 rounded-md shadow-sm">
+                  <input
+                    name="email"
+                    ref={register({ required: true })}
+                    id="email"
+                    type="email"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  />
+                </div>
+                {errors.email && (
+                  <span className="text-xs text-red-600">
+                    This field is required
+                  </span>
+                )}
+              </div>
+
+              <div className="mt-6">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-5 text-gray-700"
+                >
+                  Password
+                </label>
+                <div className="mt-1 rounded-md shadow-sm">
+                  <input
+                    name="password"
+                    ref={register({ required: true, minLength: 6 })}
+                    id="password"
+                    type="password"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  />
+                </div>
+                {errors.password && errors.password.type === 'required' && (
+                  <span className="text-xs text-red-600">
+                    This field is required
+                  </span>
+                )}
+                {errors.password && errors.password.type === 'minLength' && (
+                  <span className="text-xs text-red-600">
+                    The password must be at least 6 characters long
+                  </span>
+                )}
+              </div>
+
+              <div className="mt-10">
+                <span className="block w-full rounded-md shadow-sm">
+                  <button
+                    type="submit"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+                  >
+                    Sign Up
+                  </button>
+                </span>
+              </div>
+            </form>
+
+            <div className="mt-6">
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <div className="w-full border-t border-gray-300" />
+                </div>
+                <div className="relative flex justify-center text-sm leading-5">
+                  <span className="px-2 bg-white text-gray-500">
+                    {'Already have an account?'}
+                  </span>
+                </div>
+              </div>
+
+              <div className="mt-3">
+                <Link href="/signin">
+                  <div className="flex justify-center text-sm leading-5">
+                    <a className="cursor-pointer font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                      Click here to sign in
+                    </a>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </PublicLayout>
   )
 }
