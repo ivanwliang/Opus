7b7f8e75b717b331eed6dd13672384168c117844
Johnathan Lee <johnathanlee@Johnathans-MacBook-Pro.local>
2020-06-27 16:52:01 -0700

    connected nickname function with data fetch to generate tabs

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { useQuery } from 'react-query'
 import { format, formatISO, getYear, getMonth, startOfWeek } from 'date-fns'
@@ -7,20 +7,27 @@ import fetch from '../utils/fetch'
 import { useAuth } from '../hooks/use-auth'
 import AppLayout from '../layouts/AppLayout'
 
+// renders the tabs contents
+// TBU potentially break out into a different component
 const CategoryGoals = ({
-  category,
+  nickname,
   annualGoals,
   monthlyGoals,
   weeklyGoals
 }) => {
+  // TBU remove when this is not needed
+  if (!nickname) {
+    return (<span>Loading...</span>)
+  }
+
   return (
     <div>
       <div className="dashboard-tabcontent">
-        <h4>Annual {category} Goal:</h4>
+        <h4>Annual {nickname} Goal:</h4>
         <p>
           {
             annualGoals.filter(
-              (goal) => goal.category === category.toLowerCase()
+              (goal) => goal.nickname.toLowerCase() === nickname.toLowerCase()
             )[0].goalStatement
           }
         </p>
@@ -29,11 +36,11 @@ const CategoryGoals = ({
         </button>
       </div>
       <div className="dashboard-tabcontent">
-        <h4>Monthly {category} Goal:</h4>
+        <h4>Monthly {nickname} Goal:</h4>
         <p>
           {
             monthlyGoals.filter(
-              (goal) => goal.category === category.toLowerCase()
+              (goal) => goal.nickname.toLowerCase() === nickname.toLowerCase()
             )[0].goalStatement
           }
         </p>
@@ -42,11 +49,11 @@ const CategoryGoals = ({
         </button>
       </div>
       <div className="dashboard-tabcontent">
-        <h4>Weekly {category} Goal:</h4>
+        <h4>Weekly {nickname} Goal:</h4>
         <p>
           {
             weeklyGoals.filter(
-              (goal) => goal.category === category.toLowerCase()
+              (goal) => goal.nickname.toLowerCase() === nickname.toLowerCase()
             )[0].goalStatement
           }
         </p>
@@ -64,27 +71,24 @@ const Dashboard = () => {
 
   // state for changing "tabs" in the main dashboard
   // const [desiredGoal, setDesiredGoal] = useState('')
-  const [category, setCategory] = useState('Finance')
+  const [nickname, setNickname] = useState('')
   const [year, setYear] = useState(formatISO(Date.now()))
   const [month, setMonth] = useState(formatISO(Date.now()))
   const [week, setWeek] = useState(startOfWeek(Date.now()))
 
-  const {
-    status: annualStatus,
-    data: annualData,
-    error: annualError
-  } = useQuery('annualGoals', () => fetch(`/api/annualGoals?year=${year}`))
+  // feteching data for annual, monthly, and weekly goals
+  const {status: annualStatus, data: annualData} = useQuery('annualGoals', () => fetch(`/api/annualGoals?year=${year}`));
+  const {status: monthlyStatus, data: monthlyData} = useQuery('monthlyGoals', () => fetch(`/api/monthlyGoals`));
+  const {status: weeklyStatus, data: weeklyData} = useQuery('weeklyGoals', () => fetch(`/api/weeklyGoals`));
 
-  const { status: monthlyStatus, data: monthlyData } = useQuery(
-    'monthlyGoals',
-    () => fetch(`/api/monthlyGoals`)
-  )
+  // once the data is available, set the default nickname for the user
+  useEffect(() => {
+    if (annualData) {
+      setNickname(annualData[0].nickname);
+  }}, [annualData])
 
-  const { status: weeklyStatus, data: weeklyData } = useQuery(
-    'weeklyGoals',
-    () => fetch(`/api/weeklyGoals`)
-  )
-
+  // logic waiting for data to load before rendering the page
+  // TBU for some type of loading animation
   if (
     annualStatus === 'loading' ||
     monthlyStatus === 'loading' ||
@@ -92,19 +96,15 @@ const Dashboard = () => {
   ) {
     return <span>Loading...</span>
   }
-
-  if (annualStatus === 'error') {
-    return <span>Error: {annualError.message}</span>
-  }
-
+  
+  // TBU error page for when there are problem pulling from the database
+  // 
   return (
     <AppLayout>
-      {annualData.map((goal) => (
+      {/* {annualData && annualData.map((goal) => (
         <p key={goal.id}>{goal.goalStatement}</p>
-      ))}
-      <p>Year: {year}</p>
-      <p>Month: {month}</p>
-      <p>Week: {format(week, 'MM/dd/yyyy')}</p>
+      ))} */}
+      
       <button
         type="button"
         className="border border-cool-gray-900"
@@ -114,7 +114,7 @@ const Dashboard = () => {
       </button>
       <h1>Week of 6/21/2020</h1>
       <div className="dashboard-container">
-        {weeklyData.map((goal) => (
+        {weeklyData && weeklyData.map((goal) => (
           <div key={goal.id} className="dashboard-summary">
             <p className="dashboard-goal">
               Weekly{' '}
@@ -130,31 +130,20 @@ const Dashboard = () => {
       <div className="dashboard-container">Some chart showing analytics</div>
       <div className="dashboard-container">
         <div className="dashboard-tab">
-          <button
-            type="button"
-            className="dashboard-tablinks"
-            onClick={() => setCategory('Finance')}
-          >
-            Finance
-          </button>
-          <button
-            type="button"
-            className="dashboard-tablinks"
-            onClick={() => setCategory('Health')}
-          >
-            Health
-          </button>
-          <button
-            type="button"
-            className="dashboard-tablinks"
-            onClick={() => setCategory('Professional')}
-          >
-            Professional
-          </button>
+          {annualData && annualData.map((goal) => (
+            <button
+              key={goal.id}
+              type="button"
+              className="dashboard-tablinks"
+              onClick={() => setNickname(goal.nickname)}
+            >
+              {goal.nickname}
+            </button>
+          ))}
         </div>
 
         <CategoryGoals
-          category={category}
+          nickname={nickname}
           annualGoals={annualData}
           monthlyGoals={monthlyData}
           weeklyGoals={weeklyData}
--- utils/fetch.js
+++ utils/fetch.js
@@ -1,6 +1,7 @@
 import fetch from 'isomorphic-unfetch'
 
 // eslint-disable-next-line get-off-my-lawn/prefer-arrow-functions
+// TBU error handling need to be determined
 export default async function (...args) {
   const res = await fetch(...args)
 
